1c251e045fa40828d12e37053f0a9e50b05e275b
23navi <navisureka23@gmail.com>
2023-08-27 08:11:14 +0530

    sec1 wrapup

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAUtkVvRoUTl3lUujjN-vT10liatn8THew",
+  authDomain: "fir-1cfac.firebaseapp.com",
+  projectId: "fir-1cfac",
+  storageBucket: "fir-1cfac.appspot.com",
+  messagingSenderId: "225012700018",
+  appId: "1:225012700018:web:7e139c3d8b5fa3ab3283eb",
+  measurementId: "G-3510203LKF",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;

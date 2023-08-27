e4f3fb0191b6078fea917b570b96257d7159dcf8
23navi <navisureka23@gmail.com>
2023-08-27 08:30:02 +0530

    test update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Firebase Recipe</h1>
+        <h1>Firebase Recipe v1.0.1</h1>
       </header>
     </div>
   );

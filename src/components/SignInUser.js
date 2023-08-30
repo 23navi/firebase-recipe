da23e525e84bbcc517e32a29f6a15cce0032874c
23navi <navisureka23@gmail.com>
2023-08-30 16:49:02 +0530

    start of firestore

--- src/components/SignInUser.js
+++ src/components/SignInUser.js
@@ -5,7 +5,8 @@ export default function SignInUser({ user }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (e) => {
+    e.preventDefault();
     try {
       await FirebaseAuthService.loginUser(email, password);
       setEmail("");
@@ -17,21 +18,37 @@ export default function SignInUser({ user }) {
   };
 
   return (
-    <div>
-      <h1>SignIn User</h1>
-      <input
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Register</button>
+    <div className="login-form-container">
+      <h3>Welcome Back! Signin User</h3>
+      <form onSubmit={handleSignIn} className="login-form">
+        <label className="input-label login-label">
+          Email:
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-text"
+            required
+          />
+        </label>
+
+        <label className="input-label password-label">
+          Password:
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input-text"
+            required
+          />
+        </label>
+
+        <button className="input-button" type="submit" onClick={handleSignIn}>
+          Signin
+        </button>
+      </form>
     </div>
   );
 }
